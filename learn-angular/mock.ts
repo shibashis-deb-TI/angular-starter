509bd7fc7ddabc6db3067325cc66ae531665ea7f
Shibashis Deb <shibashis.deb@technoidentity.com>
2024-04-25 11:35:18 +0530

    minor changes

---
+++ learn-angular/mock.ts
@@ -0,0 +1,27 @@
+// Mock data to restore in db
+
+export const donuts = [
+  {
+    id: 'y8z@As',
+    name: 'Just Chocolate',
+    price: 119,
+    icon: 'just-chocolate',
+    promo: 'new',
+    description: 'For the pure chocoholic.',
+  },
+  {
+    id: '3u98K1',
+    name: 'Glazed Fudge',
+    icon: 'glazed-fudge',
+    price: 129,
+    description: 'Sticky perfection.',
+    promo: 'limited',
+  },
+  {
+    id: 'ae098s',
+    name: 'Caramel Swirl',
+    price: 129,
+    icon: 'caramel-swirl',
+    description: 'Sticky perfection.',
+  },
+];
