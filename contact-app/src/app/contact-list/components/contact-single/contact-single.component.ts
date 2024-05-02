bcc5cbe45e9aa4764cf7c19498ccd5c789706652
Shibashis Deb <shibashis.deb@technoidentity.com>
2024-05-02 10:55:17 +0530

    add styles to form; add validation

--- contact-app/src/app/contact-list/components/contact-single/contact-single.component.ts
+++ contact-app/src/app/contact-list/components/contact-single/contact-single.component.ts
@@ -1,6 +1,7 @@
 import { Component, EventEmitter, Input, Output } from '@angular/core'
 import { NgForm } from '@angular/forms'
 import { Contact } from '../../models/contact.model'
+import { Router } from '@angular/router'
 
 @Component({
   selector: 'app-contact-form',
@@ -13,12 +14,28 @@ export class ContactSingleComponent {
   @Output() create = new EventEmitter<Omit<Contact, 'id'>>()
   @Output() update = new EventEmitter<Contact>()
 
+  constructor(private router: Router) {}
+
   onSubmit(form: NgForm) {
+    if (form.invalid) {
+      return
+    }
+
     if (this.contact) {
       this.update.emit({ id: this.contact.id, ...form.value })
       return
     }
 
     this.create.emit(form.value)
+  }
+
+  handleUpdate(form: NgForm) {
+    if (!form.form.pristine) {
+      return this.onSubmit(form)
+    }
+
+    const goBack = confirm('Save without any changes?')
+
+    return goBack ? this.router.navigate(['contact-list']) : undefined
   }
 }
